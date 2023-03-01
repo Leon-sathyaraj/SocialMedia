7218b40038ef9b1de10031a0e5baad6af8334c80
Leon <sathyarajthevar@gmail.com>
2023-03-01 16:59:31 +0800

    committing for post
    incomplete but committing so that i can work on another device

--- Client/src/pages/components/post/Post.jsx
+++ Client/src/pages/components/post/Post.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../post/Post.scss'
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
-const Post = () => {
+const Post = ({post}) => {
     return (
         <div className='post'>
-        Post
-            
+        <div className='user'>
+            <div className='userInfo'>
+            <div className='details'>
+                <Link to={`/profile/${post.userId}`} style={{textDecoration: "none", color:"inherit"}}>
+                    <span>{post.name}</span>
+                </Link>
+            </div>
+            </div>
+            <MoreHorizIcon/>
+        </div> 
+        <div className='content'>
+            <div className='info'></div>
+        </div>
         </div>
     );
 };
