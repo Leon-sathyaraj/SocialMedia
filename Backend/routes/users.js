419e4c0de25362d003d25162c1f0e04ccca58b22
Leon <sathyarajthevar@gmail.com>
2023-03-02 17:22:46 +0800

    set up a sample route and its working

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getUser, updateUser } from "../controllers/user.js";
+// import { getUser, updateUser } from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get("/find/:userId", getUser);
-router.put("/", updateUser);
+router.get("/test",(req,res)=> {
+    res.send("testing and it works")
+})
+
+// router.get("/find/:userId", getUser);
+// router.put("/", updateUser);
 
 export default router;
