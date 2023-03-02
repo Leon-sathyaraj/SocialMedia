44e5e36412f42086c33e63936111e0642bec51b5
Leon <sathyarajthevar@gmail.com>
2023-03-02 18:36:48 +0800

    some codes to connect to sql
    committing to pull and work on other device

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,7 +1,17 @@
+import { db } from "../connect"
+
 export const login = (req,res) =>{
 
 
+const q = "SELECT FROM users where username = ?"
 
+db.query(q,[req.body.username], (err,data)=>{
+    if(err) return res.status(500).json(err)
+    if(data.length) return res.status(409).son("User is already taken! ")
+})
+
+const salt = bcrypt.genSaltSync(10);
+const hashedPassword = bcrypt.hashSync(req.body.password, salt)
     
 }
 
--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
-import Express from "express";
-const app = Express()
+import express from "express";
+const app = express()
 
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
@@ -7,6 +7,8 @@ import postRoutes from './routes/posts.js'
 import commentRoutes from './routes/comments.js'
 import likeRoutes from './routes/likes.js'
 
+//middlewares
+app.use(express.json())
 
 app.use("/api/users", userRoutes)
 app.use("/api/users", postRoutes)
