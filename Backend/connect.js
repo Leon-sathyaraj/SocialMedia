4ea9394bc6d8cd2e028c51da83444058b258541a
Leon <sathyarajthevar@gmail.com>
2023-03-06 14:11:22 +0800

    commit

--- Backend/connect.js
+++ Backend/connect.js
@@ -3,11 +3,11 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "123456789",
+  password: "dsaewq321@",
   database: "social",
 });
 
-// db.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
-//   if (error) throw error;
-//   console.log("The solution is: ", results[0].solution);
-// });
+db.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+  if (error) throw error;
+  console.log("The solution is: ", results[0].solution);
+});
--- Client/src/pages/register/Register.jsx
+++ Client/src/pages/register/Register.jsx
@@ -1,35 +1,44 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import "./Register.scss";
-import axios from "axios";
-
+import {useState} from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+import './Register.scss'
 const Register = () => {
+
   const [inputs, setInputs] = useState({
-    username: "",
-    email: "",
-    password: "",
-    name: "",
-  });
-  const [err, setErr] = useState(null);
+    username: '',
+    email: '',
+    password:'',
+    name:''
+  })
 
-  const handleChange = (e) => {
-    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
+  const [error, setError] = useState({
+    username: '',
+    email: '',
+    password:'',
+    name:''
+  })
 
-  const handleClick = async (e) => {
-    e.preventDefault();
+  const handleChange = e => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value}))
+  }
 
-    try {
-      await axios.post("http://localhost:8800/api/auth/register", inputs);
-    } catch (err) {
-      setErr(err.response.data);
+  const handleClick =  async e => {
+    e.preventDefault()
+
+    try{
+      await axios.post('http://localhost:8800/api/auth/register', inputs )
+    } catch(error) {
+      setError(true)
     }
   };
 
-  console.log(err)
+  console.log(error)
+
+
 
   return (
     <div className="register">
+<<<<<<< HEAD
       <div className="card">
         <div className="left">
           <h1>Friends Book</h1>
@@ -73,8 +82,32 @@ const Register = () => {
             <button onClick={handleClick}>Register</button>
           </form>
         </div>
+=======
+    <div className="card">
+      <div className="left">
+        <h1> Welcome to our Friendsbook,
+         where you can connect and share with friends and family from around the world! 
+    </h1>
+        <p>
+        {/* Welcome to Friends Book */}
+        </p>
+        <span>Do you have an account?</span>
+        <Link to ="/login">  <button>Login</button></Link>
+      </div>
+      <div className="right">
+        <h1>Register</h1>
+        <form>
+          <input type="text" placeholder="Username" name="username" onChang={handleChange} />
+          <input type="email" placeholder="Email"  name="email" onChang={handleChange} />
+          <input type="password" placeholder="Password"  name="password" onChang={handleChange} />
+          <input type="text" placeholder="Name"  name="name" onChang={handleChange} />
+          {error && error}
+          <button onClick={handleClick}>Register</button>
+        </form>
+>>>>>>> parent of 1b753d5 (Sql working and Register creation works with backend with validation)
       </div>
     </div>
+  </div>
   );
 };
 
