adcfa0746960caf419fbf65f545d4f89246b2a66
Leon <sathyarajthevar@gmail.com>
2023-03-06 14:15:22 +0800

    Update Register.jsx

--- Client/src/pages/register/Register.jsx
+++ Client/src/pages/register/Register.jsx
@@ -1,87 +1,67 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import "./Register.scss";
+import {useState} from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+import './Register.scss'
 const Register = () => {
+
   const [inputs, setInputs] = useState({
-    username: "",
-    email: "",
-    password: "",
-    name: "",
-  });
+    username: '',
+    email: '',
+    password:'',
+    name:''
+  })
 
   const [error, setError] = useState({
-    username: "",
-    email: "",
-    password: "",
-    name: "",
-  });
+    username: '',
+    email: '',
+    password:'',
+    name:''
+  })
 
-  const handleChange = (e) => {
-    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
+  const handleChange = e => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value}))
+  }
 
-  const handleClick = async (e) => {
-    e.preventDefault();
+  const handleClick =  async e => {
+    e.preventDefault()
 
-    try {
-      await axios.post("http://localhost:8800/api/auth/register", inputs);
-    } catch (error) {
-      setError(true);
+    try{
+      await axios.post('http://localhost:8800/api/auth/register', inputs )
+    } catch(error) {
+      setError(true)
     }
   };
 
-  console.log(error);
+  console.log(error)
+
+
 
   return (
     <div className="register">
-      <div className="card">
-        <div className="left">
-          <h1>
-            {" "}
-            Welcome to our Friendsbook, where you can connect and share with
-            friends and family from around the world!
-          </h1>
-          <p>{/* Welcome to Friends Book */}</p>
-          <span>Do you have an account?</span>
-          <Link to="/login">
-            {" "}
-            <button>Login</button>
-          </Link>
-        </div>
-        <div className="right">
-          <h1>Register</h1>
-          <form>
-            <input
-              type="text"
-              placeholder="Username"
-              name="username"
-              onChang={handleChange}
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChang={handleChange}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChang={handleChange}
-            />
-            <input
-              type="text"
-              placeholder="Name"
-              name="name"
-              onChang={handleChange}
-            />
-            {error && error}
-            <button onClick={handleClick}>Register</button>
-          </form>
-        </div>
+    <div className="card">
+      <div className="left">
+        <h1> Welcome to our Friendsbook,
+         where you can connect and share with friends and family from around the world! 
+    </h1>
+        <p>
+        {/* Welcome to Friends Book */}
+        </p>
+        <span>Do you have an account?</span>
+        <Link to ="/login">  <button>Login</button></Link>
+      </div>
+      <div className="right">
+        <h1>Register</h1>
+        <form>
+          <input type="text" placeholder="Username" name="username" onChang={handleChange} />
+          <input type="email" placeholder="Email"  name="email" onChang={handleChange} />
+          <input type="password" placeholder="Password"  name="password" onChang={handleChange} />
+          <input type="text" placeholder="Name"  name="name" onChang={handleChange} />
+          {error && error}
+          <button onClick={handleClick}>Register</button>
+        </form>
       </div>
     </div>
+  </div>
   );
 };
 
