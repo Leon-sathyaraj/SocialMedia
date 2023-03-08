2d6bec61eee06ae2506ce49101c804e4ea496743
Leon <sathyarajthevar@gmail.com>
2023-03-08 11:17:37 +0800

    Profile and cover picture working

--- Backend/connect.js
+++ Backend/connect.js
@@ -7,4 +7,11 @@ export const db = mysql.createConnection({
   database: "social",
 });
 
+db.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
 
+  console.log('connected as id ' + db.threadId);
+});
